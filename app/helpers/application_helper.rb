b5d949fff498130e6312527395929729199a7158
Ivan Brilata <brilataivan86@gmail.com>
2024-12-30 00:58:44 +0800

    latest update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def van(email, size: 64, alt: nil, class_name: nil)
+    image_path = "van.jpg"
+    alt_text = alt || "Image for #{email}"
+    
+    image_tag(image_path, alt: alt_text, class: class_name, style: "width: #{size}px; height: #{size}px;")
+  end
 end
